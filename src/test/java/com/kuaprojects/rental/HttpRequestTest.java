cd062d7f47934de42a1c00c91496e6497fcb9ed2
Arminas42 <72023124+Arminas42@users.noreply.github.com>
2024-04-16 17:24:57 +0300

    Added sanityCheck endpoint, http request test

---
+++ src/test/java/com/kuaprojects/rental/HttpRequestTest.java
@@ -0,0 +1,25 @@
+package com.kuaprojects.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void sanityCheckShouldReturnDefaultMessage(){
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/sanityCheck",
+                String.class)).contains("Alive");
+    }
+}
